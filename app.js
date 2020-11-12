13b5aeeb6b0a730593eaf78ba6b1e435ec3a7f05
Nicolas <nicolasrodriguezch@hotmail.com>
2020-11-12 16:49:15 -0500

    updates app.js

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-    const nombre = req.query.nombre;
+// app.get("/", (req, res) => {
+//     const nombre = req.query.nombre;
 
+//     if (nombre === "" || nombre === undefined) {
+//         res.send(`<h1>Hola desconocido!</h1>`)
+//     } else {
+//         res.send(`<h1>Hola ${nombre}!</h1>`)
+//     }
+// });
+
+app.get("/makers/:nombre", (req, res) => {
+    const nombre = req.params.nombre
+
+    capitalizeFirstLetter = function (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+    }
     if (nombre === "" || nombre === undefined) {
         res.send(`<h1>Hola desconocido!</h1>`)
     } else {
-        res.send(`<h1>Hola ${nombre}!</h1>`)
+        res.send(`<h1>Hola ${capitalizeFirstLetter(nombre)}!</h1>`)
     }
-});
+}); 
 
 app.listen(3000, ()=> console.log("Listening on port 3000"));
