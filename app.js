11e06838709ecb723429c6ef7ee2ece581b10562
Nicolas <nicolasrodriguezch@hotmail.com>
2020-11-12 16:00:24 -0500

    creates app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+    const nombre = req.query.nombre;
+
+    if (nombre === "" || nombre === undefined) {
+        res.send(`<h1>Hola desconocido</h1>`)
+    } else {
+        res.send(`<h1>Hola ${nombre}</h1>`)
+    }
+});
+
+app.listen(3000, ()=> console.log("Listening on port 3000"));
